e305a44ddbe92a1871b40c5382eb0c41dbeb868d
edammame <139234613+edammame@users.noreply.github.com>
2024-02-20 14:41:00 +0700

    ini commit pertama

---
+++ client/src/app/event/page.jsx
@@ -0,0 +1,10 @@
+import { NavbarEvent } from "@/components/navbarevent";
+
+function Event() {
+  return (
+    <>
+      <NavbarEvent />
+    </>
+  );
+}
+export default Event;
---
+++ client/src/redux/slices/userSlice.js
@@ -0,0 +1,27 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  email: "",
+  id: 0,
+  name: "",
+};
+
+const userSlice = createSlice({
+  name: "user",
+  initialState,
+  reducers: {
+    functionLogin: (state, action) => {
+      return {
+        ...state,
+        ...action.payload,
+      };
+    },
+    functionLogout: (state) => {
+      localStorage.removeItem("user");
+      return initialState;
+    },
+  },
+});
+
+export const { functionLogin, functionLogout } = userSlice.actions;
+export default userSlice.reducer;
